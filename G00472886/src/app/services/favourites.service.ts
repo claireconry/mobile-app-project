cc8be93f1f1a194aa9b280bb4808a4f62fd9954c
cl41ire <G00472886@atu.ie>
2026-05-02 11:16:13 +0100

    favourites service

--- G00472886/src/app/services/favourites.service.ts
+++ G00472886/src/app/services/favourites.service.ts
@@ -4,6 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FavouritesService {
+  private favourites: any[] = [];
 
-  constructor() { }
+  constructor() {
+    const saved = localStorage.getItem('favourites');
+    if (saved) {
+      this.favourites = JSON.parse(saved);
+    }
+  }
+
+  getFavourites() {
+    return this.favourites;
+  }
+
+  addFavourite(movie: any) {
+    this.favourites.push(movie);
+    localStorage.setItem('favourites', JSON.stringify(this.favourites));
+  }
+
+  removeFavourite(movieId: number) {
+    this.favourites = this.favourites.filter(m => m.id !== movieId);
+    localStorage.setItem('favourites', JSON.stringify(this.favourites));
+  }
+
+  isFavourite(movieId: number) {
+    return this.favourites.some(m => m.id === movieId);
+  }
 }
