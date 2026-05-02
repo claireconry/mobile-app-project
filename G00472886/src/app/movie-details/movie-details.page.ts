49d5df078bfb5c890fc1906e69a96fc25b4faa9a
cl41ire <G00472886@atu.ie>
2026-05-02 11:11:00 +0100

    movie details page

--- G00472886/src/app/movie-details/movie-details.page.ts
+++ G00472886/src/app/movie-details/movie-details.page.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { FavouritesService } from '../services/favourites.service';
+import { Router, RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { heart, home } from 'ionicons/icons';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonIcon, IonButtons, CommonModule, RouterLink]
 })
 export class MovieDetailsPage implements OnInit {
+  movie: any;
+  cast: any[] = [];
+  crew: any[] = [];
+  isFavourite: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private movieService: MovieService,
+    private favouritesService: FavouritesService,
+    private router: Router
+  ) {
+    addIcons({ heart, home });
   }
 
+  ngOnInit() {
+    this.movie = history.state.movie;
+    if (this.movie) {
+      this.movieService.getMovieCredits(this.movie.id).subscribe((data: any) => {
+        this.cast = data.cast;
+        this.crew = data.crew;
+      });
+      this.isFavourite = this.favouritesService.isFavourite(this.movie.id);
+    }
+  }
+
+  toggleFavourite() {
+    if (this.isFavourite) {
+      this.favouritesService.removeFavourite(this.movie.id);
+    } else {
+      this.favouritesService.addFavourite(this.movie);
+    }
+    this.isFavourite = !this.isFavourite;
+  }
+
+  goToDetails(person: any) {
+    this.router.navigate(['/details'], { state: { person } });
+  }
 }
