9536bc9174e72885be326cd1a6d406bc6968dba2
cl41ire <G00472886@atu.ie>
2026-05-02 09:52:09 +0100

    getMovieCredits function added to movie service

--- G00472886/src/app/services/movie.service.ts
+++ G00472886/src/app/services/movie.service.ts
@@ -5,6 +5,29 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MovieService {
+  private apiKey = '21acb0955401e4c44da8304d6998b95f';
+  private baseUrl = 'https://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) {}
+
+  getTrendingMovies() {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`);
+  }
+
+  searchMovies(query: string) {
+    return this.http.get(`${this.baseUrl}/search/movie?query=${query}&api_key=${this.apiKey}`);
+  }
+
+  getMovieCredits(movieId: number) {
+    return this.http.get(`${this.baseUrl}/movie/${movieId}/credits?api_key=${this.apiKey}`);
+  }
+}import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
    private apiKey = '21acb0955401e4c44da8304d6998b95f';
    private baseUrl = 'https://api.themoviedb.org/3';
 
