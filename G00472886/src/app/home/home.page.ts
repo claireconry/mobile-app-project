e642fd021461417bb6d5789a6d55492c14158337
cl41ire <G00472886@atu.ie>
2026-05-02 09:28:48 +0100

    Home page shows movies, API and search working

--- G00472886/src/app/home/home.page.ts
+++ G00472886/src/app/home/home.page.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonIcon, IonButtons, IonSearchbar } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from '../services/movie.service';
+import { Router, RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonIcon, IonButtons, IonSearchbar, CommonModule, FormsModule, RouterLink],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  movies: any[] = [];
+  searchQuery: string = '';
+
+  constructor(private movieService: MovieService, private router: Router) {
+    addIcons({ heart });
+  }
+
+  ngOnInit() {
+    this.loadTrendingMovies();
+  }
+
+  loadTrendingMovies() {
+    this.movieService.getTrendingMovies().subscribe((data: any) => {
+      this.movies = data.results;
+    });
+  }
+
+  searchMovies() {
+    if (this.searchQuery.trim() === '') {
+      this.loadTrendingMovies();
+    } else {
+      this.movieService.searchMovies(this.searchQuery).subscribe((data: any) => {
+        this.movies = data.results;
+      });
+    }
+  }
+
+  goToMovieDetails(movie: any) {
+    this.router.navigate(['/movie-details'], { state: { movie } });
+  }
 }
