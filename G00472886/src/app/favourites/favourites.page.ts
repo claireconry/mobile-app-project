35d35cb966474d78d0ae0c9bbd80eb86a9d98520
cl41ire <G00472886@atu.ie>
2026-05-04 10:34:20 +0100

    favourites page typescript

--- G00472886/src/app/favourites/favourites.page.ts
+++ G00472886/src/app/favourites/favourites.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { FavouritesService } from '../services/favourites.service';
+import { MovieService } from '../services/movie.service';
+import { Router, RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { home } from 'ionicons/icons';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonIcon, IonButtons, CommonModule, RouterLink]
 })
 export class FavouritesPage implements OnInit {
+  favourites: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private favouritesService: FavouritesService,
+    private movieService: MovieService,
+    private router: Router
+  ) {
+    addIcons({ home });
   }
 
+  ngOnInit() {
+    this.favourites = this.favouritesService.getFavourites();
+  }
+
+  goToMovieDetails(movie: any) {
+    this.movieService.selectedMovie = movie;
+    this.router.navigate(['/movie-details']);
+  }
 }
